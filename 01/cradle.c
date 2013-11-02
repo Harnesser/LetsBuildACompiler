3263c1e4c67b5aa6250dc6b32f04987229031940
Marty <harnesser+kartoffel@gmail.com>
2013-11-02 21:15:33 +0000

    cradle.c copmiling

---
+++ 01/cradle.c
@@ -0,0 +1,103 @@
+// compiler cradle program
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+const int TAB = '\t';
+const int MAXMSG = 100;
+
+char Look;  /* lookahead character */
+
+void GetChar(void)
+{
+	Look = getchar();
+}
+
+void Error(const char *msg)
+{
+	printf("\n");
+	printf("Error: %s.\n", msg);
+}
+
+void Abort(const char *msg)
+{
+	Error(msg);
+	exit(1);
+}
+
+void Expected(const char *msg)
+{
+	char msgl[MAXMSG];
+	strncpy(msgl, msg, MAXMSG);
+	strncat(msgl, " Expected", MAXMSG);
+	Abort(msgl);
+}
+
+void Match(const char tok)
+{
+	char msg[MAXMSG];
+	if (Look == tok) {
+		GetChar();
+	} else {
+		snprintf(msg, MAXMSG, "\"%c\"", tok);
+		Expected(msg);
+	}
+}
+
+int IsAlpha(const char tok)
+{
+	return isalnum(tok);
+}
+
+int IsDigit(const char tok)
+{
+	return isdigit(tok);
+}
+
+char GetName(void)
+{
+	char name;
+	if (!IsAlpha(Look)) {
+		Expected("Name");
+	}
+	name = toupper(Look);
+	GetChar();
+	return name;
+}
+
+char GetNum(void)
+{
+	char num;
+	if (!IsDigit(Look)) {
+		Expected("Integer");
+	}
+	num = Look;
+	GetChar();
+}
+
+void Emit(const char *msg)
+{
+	printf("\t%s", msg);
+}
+
+void EmitLn(const char *msg)
+{
+	Emit(msg);
+	printf("\n");
+}
+
+void Init(void)
+{
+	GetChar();
+}
+
+
+/* -------------------------------------------------------------------- */
+int main(int argc, char *argv[])
+{
+	Init();
+}
+
+
+
