2476b42af1b394f7b2b775289e11e8d5656f80fe
Marty <harnesser+kartoffel@gmail.com>
2013-11-03 20:27:30 +0000

    Dissasemble a simple C program for the laugh

---
+++ assembly/gcc/simple_assignment.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int a,b,c,d,e,f,g,h,i,j,k;
+	a = 0x1A;
+	b = 0x1B;
+	c = 0x1C;
+	d = 0x1D;
+	e = 0x1E;
+	f = 0x1F;
+	g = 0x20;
+	h = 0x21;
+	i = 0x22;
+	j = 0x23;
+	k = 0x24;
+	printf("a=%d, b=%d\n",a, b);
+	return 0;
+}
