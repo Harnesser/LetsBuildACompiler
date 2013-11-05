7bfae76a1f377828945a219ee5b60b49d9dfc457
Marty <harnesser+kartoffel@gmail.com>
2013-11-05 00:08:40 +0000

    script to restore dev windows

---
+++ windows.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env sh
+
+PRJDIR=/home/harnesser/Projects/LetsBuildACompiler
+TUTORIAL=04
+FILENAME=interpreter.c
+
+# can't use command as terminal exits once it's done :(
+#		--command="make"
+#		--command="vi ${FILENAME}" 
+
+gnome-terminal \
+	--tab-with-profile=DEFAULT \
+	--geometry=90x100+900+10 \
+		--title=SRC \
+		--working-directory=${PRJDIR}/${TUTORIAL} \
+	--tab-with-profile=DEFAULT \
+		--title=Test \
+		--working-directory=${PRJDIR}/${TUTORIAL} &
+
+tut=$(expr $TUTORIAL + 0)
+gedit \
+	--background \
+	--geometry=60x120+100+10 \
+	--new-window \
+	${PRJDIR}/tutorials/tutor${tut}.txt &
+
+
