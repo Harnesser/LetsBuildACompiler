8e3dc2a210b0f9ac44c2f77177b6d96bae76ebbb
Marty <harnesser+lt4@gmail.com>
2014-01-09 21:47:24 +0000

    TDOP: finished python example

--- TDOP/Bendersky/tdop.py
+++ TDOP/Bendersky/tdop.py
@@ -12,10 +12,19 @@ def tokenize(program):
 			yield literal_token(number)
 		elif operator == '+':
 			yield operator_add_token()
+		elif operator == '-':
+			yield operator_sub_token()
 		elif operator == '*':
 			yield operator_mul_token()
+		elif operator == '^':
+			yield operator_pow_token()
+		elif operator == '(':
+			yield operator_lparen_token()
+		elif operator == ')':
+			yield operator_rparen_token()
 		else:
-			raise SyntaxError('uknown operator %s' % operator)
+			msg = "unknown operator '%s'" % (operator) 
+			raise SyntaxError(msg)
 	yield end_token()
 
 def parse(program):
@@ -25,6 +34,14 @@ def parse(program):
 	token = next()
 	indent = 0
 	return expression()
+
+def match(tok=None):
+	global token
+	print type(token)
+	if tok and tok != type(token):
+		msg = "expected '%s'" % (tok)
+		raise SyntaxError(msg)
+	token = next()
 
 def mesg(message):
 	global indent
@@ -68,11 +85,70 @@ class operator_mul_token(object):
 		mesg("led of '*'")
 		return left * expression(20)
 
+class operator_sub_token(object):
+	lbp = 10
+	def nud(self):
+		mesg("nud of '-'")
+		return -expression(100)
+	def led(self, left):
+		mesg("led of '-'")
+		return left - expression(10)
+
+class operator_pow_token(object):
+	""" Right-associative.
+	Want parser to treat subsequent exponential operators as
+	sub-expressions of the first one. So call expression() 
+	with a rbp lower that the lbp of the exponential.
+
+	When expression() gets to next '^', it finds that still
+	rbp < token.lbp and won't return the result right away.
+	but collects the value of the sub-expression first.
+	"""
+	lbp = 30
+	def led(self, left):
+		mesg("led of '^'")
+		return left ** expression(30-1)
+
+class operator_lparen_token(object):
+	lbp = 0
+	def nud(self):
+		mesg("nud of '('")
+		expr = expression()
+		match(operator_rparen_token)
+		return expr
+
+class operator_rparen_token(object):
+	lbp = 0
+
 class end_token(object):
 	lbp = 0
 
+def check_expression(program, expected):
+	print "PROGRAM: %s" % (program)
+	value = parse(program)
+	print "VALUE: %d" % (value)
+	if value != expected:
+		print "FAIL!!!"
+		return 1
+	else:
+		return 0
+
+tests = [
+	( "10 + 4*2", 18),
+	( "3 + 1 * 2 * 4 + 5", 16),
+	( "10 - 2 - 1", 7),
+	( "-3 - -6", 3),
+	( "3 -2 + 4 * -5", -19),
+	( "2^3", 8 ),
+	( "2^3^2", 512),
+	( "( 10 + 4) *2", 28),
+	( "( (3 + 1) * 2 * (4 + 5) )", 72),
+]	
 
 if __name__ == '__main__':
-	print parse("10 + 4*2")
-	print parse("3 + 1 * 2 * 4 + 5")
-	
+	ctests = 0
+	cfails = 0
+	for program, expected in tests:
+		ctests += 1
+		cfails += check_expression(program, expected)
+	print "%d tests, %d fails" % (ctests, cfails)
